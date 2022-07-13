36ffa847dee1268ec57793e962a3d85ee6f89672
boyknight <boyknight@126.com>
2022-07-13 09:03:33 +0800

    random

--- visiable/tensorboardDemo/demo.py
+++ visiable/tensorboardDemo/demo.py
@@ -1,24 +1,52 @@
 # imports
 import matplotlib.pyplot as plt
 import numpy as np
-
 import torch
 import torchvision
 import torchvision.transforms as transforms
-
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils.tensorboard import SummaryWriter
+import shutil  # shell util
+# from tensorboard import version
+import tensorboard as tb
+# import tensorflow as tf  # tensorflow 必须是2 版本
+
+# 解决AttributeError: module ‘tensorflow._api.v2.io.gfile’ has no attribute 'get_filesystem’错误
+# tf.io.gfile = tb.compat.tensorflow_stub.io.gfile
+# 解决方案2 就是 pytorch的环境 不要装tf  tf的环境不要装pytorch
+
 from model import Net
 
-from tensorboard import version
-
-print(version.VERSION)
-from torch.utils.tensorboard import SummaryWriter
+# print('tensorboard', version.VERSION)
+# print('tf', tf.__version__)
 
 # constant for classes
 classes = ('T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
            'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle Boot')
+
+# transforms
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5,), (0.5,))])
+
+# datasets 统一一下目录 ../../../../Data   ~/Data,~/GitCode/DeepLearning/visiable/tensorboardDemo
+trainset = torchvision.datasets.FashionMNIST('../../../../Data',
+                                             download=False,
+                                             train=True,
+                                             transform=transform)
+testset = torchvision.datasets.FashionMNIST('../../../../Data',
+                                            download=False,
+                                            train=False,
+                                            transform=transform)
+
+# dataloaders
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=0)
+
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=0)
 
 
 # helper function to show an image
@@ -34,57 +62,167 @@ def matplotlib_imshow(img, one_channel=False):
         plt.imshow(np.transpose(npimg, (1, 2, 0)))
 
 
-def main():
-    # transforms
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5,), (0.5,))])
+# 定义网络
+net = Net()
 
-    # datasets 统一一下目录 ../../../../Data   ~/Data,~/GitCode/DeepLearning/visiable/tensorboardDemo
-    trainset = torchvision.datasets.FashionMNIST('../../../../Data',
-                                                 download=False,
-                                                 train=True,
-                                                 transform=transform)
-    testset = torchvision.datasets.FashionMNIST('../../../../Data',
-                                                download=False,
-                                                train=False,
-                                                transform=transform)
+# 定义损失函数和优化器
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    # dataloaders
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                              shuffle=True, num_workers=2)
+################ 1. TensorBoard setup
+writer_path = 'runs/fashion_mnist_experiment_1'
+# 建议加一步，先把目录清空,shutil可以强制删除，os没法强制删除
+shutil.rmtree(writer_path)
+writer = SummaryWriter(writer_path)
 
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                             shuffle=False, num_workers=2)
+################ 2. Writing to TensorBoard
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
 
-    # 定义网络
-    net = Net()
+# create grid of images
+img_grid = torchvision.utils.make_grid(images)
 
-    # 定义损失函数和优化器
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+# show images
+matplotlib_imshow(img_grid, one_channel=True)
 
-    ################ 1. TensorBoard setup
-    writer = SummaryWriter('runs/fashion_mnist_experiment_1')
+# write to tensorboard
+writer.add_image('four_fashion_mnist_images', img_grid)
 
-    ################ 2. Writing to TensorBoard
-    # get some random training images
-    dataiter = iter(trainloader)
-    images, labels = dataiter.next()
+## 完了之后启动 tensorboard
+# tensorboard --logdir=runs
+# 注意要进入到项目目录下，runs是对应的输出目录 SummaryWriter('runs/fashion_mnist_experiment_1')
 
-    # create grid of images
-    img_grid = torchvision.utils.make_grid(images)
-
-    # show images
-    matplotlib_imshow(img_grid, one_channel=True)
-
-    # write to tensorboard
-    writer.add_image('four_fashion_mnist_images', img_grid)
-
-    ## 完了之后启动 tensorboard
-    # tensorboard --logdir=runs
-    # 注意要进入到项目目录下，runs是对应的输出目录 SummaryWriter('runs/fashion_mnist_experiment_1')
+############### 3. 可视化模型，这个牛逼了,这个时候的面板上面多了一个GRAPHS 页
+writer.add_graph(net, images)
+writer.close()
 
 
-if __name__ == "__main__":
-    main()
+############### 4. 高维的数据，用低维度数据表现，通过 add_embedding 方法
+# helper function
+def select_n_random(data, labels, n=100):
+    '''
+    Selects n random datapoints and their corresponding labels from a dataset
+    '''
+    assert len(data) == len(labels)
+
+    perm = torch.randperm(len(data))
+    return data[perm][:n], labels[perm][:n]
+
+
+# select random images and their target indices
+images, labels = select_n_random(trainset.data, trainset.targets)
+
+# get the class labels for each image
+class_labels = [classes[lab] for lab in labels]
+
+# log embeddings
+features = images.view(-1, 28 * 28)  # 784维的数据降维到了3维空间,RGB
+writer.add_embedding(features,
+                     metadata=class_labels,
+                     label_img=images.unsqueeze(1))
+writer.close()
+
+
+############### 5.跟踪模型训练进程
+# helper functions
+def images_to_probs(net, images):
+    '''
+    Generates predictions and corresponding probabilities from a trained
+    network and a list of images
+    '''
+    output = net(images)
+    # convert output probabilities to predicted class
+    _, preds_tensor = torch.max(output, 1)
+    preds = np.squeeze(preds_tensor.numpy())
+    return preds, [F.softmax(el, dim=0)[i].item() for i, el in zip(preds, output)]
+
+
+def plot_classes_preds(net, images, labels):
+    '''
+    Generates matplotlib Figure using a trained network, along with images
+    and labels from a batch, that shows the network's top prediction along
+    with its probability, alongside the actual label, coloring this
+    information based on whether the prediction was correct or not.
+    Uses the "images_to_probs" function.
+    '''
+    preds, probs = images_to_probs(net, images)
+    # plot the images in the batch, along with predicted and true labels
+    fig = plt.figure(figsize=(12, 48))
+    for idx in np.arange(4):
+        ax = fig.add_subplot(1, 4, idx + 1, xticks=[], yticks=[])
+        matplotlib_imshow(images[idx], one_channel=True)
+        ax.set_title("{0}, {1:.1f}%\n(label: {2})".format(
+            classes[preds[idx]],
+            probs[idx] * 100.0,
+            classes[labels[idx]]),
+            color=("green" if preds[idx] == labels[idx].item() else "red"))
+    return fig
+
+
+running_loss = 0.0
+for epoch in range(1):  # loop over the dataset multiple times
+    for i, data in enumerate(trainloader, 0):
+        print('i = ', i)
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        print('running_loss = ', running_loss)
+        if i % 1000 == 999:  # every 1000 mini-batches...
+            # ...log the running loss
+            writer.add_scalar('training loss',
+                              running_loss / 1000,
+                              epoch * len(trainloader) + i)
+
+            # ...log a Matplotlib Figure showing the model's predictions on a
+            # random mini-batch
+            writer.add_figure('predictions vs. actuals',
+                              plot_classes_preds(net, inputs, labels),
+                              global_step=epoch * len(trainloader) + i)
+            running_loss = 0.0
+# writer.close()
+print('Finished Training')
+class_probs = []
+class_label = []
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        output = net(images)
+        class_probs_batch = [F.softmax(el, dim=0) for el in output]
+
+        class_probs.append(class_probs_batch)
+        class_label.append(labels)
+
+test_probs = torch.cat([torch.stack(batch) for batch in class_probs])
+test_label = torch.cat(class_label)
+
+
+# helper function
+def add_pr_curve_tensorboard(class_index, test_probs, test_label, global_step=0):
+    '''
+    Takes in a "class_index" from 0 to 9 and plots the corresponding
+    precision-recall curve
+    '''
+    tensorboard_truth = test_label == class_index
+    tensorboard_probs = test_probs[:, class_index]
+
+    writer.add_pr_curve(classes[class_index],
+                        tensorboard_truth,
+                        tensorboard_probs,
+                        global_step=global_step)
+    writer.close()
+
+
+# plot all the pr curves
+for i in range(len(classes)):
+    add_pr_curve_tensorboard(i, test_probs, test_label)
